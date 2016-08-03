c52a787e893b749f56e8019f0cb091ad7c9df3fc
aswath93 <aswath93@gmail.com>
2016-08-03 12:12:59 -0500

    obstacle avoidance working code

--- src/obs_avoid.cpp
+++ src/obs_avoid.cpp
@@ -9,17 +9,20 @@
 using namespace DJI::onboardSDK;
 
 std::vector<float> obs_dist;
-/*
+
 void get_dist(const sensor_msgs::LaserScan::ConstPtr& dist)
 {
     obs_dist=dist->ranges;
-    ROS_INFO("inside callbak");
-    ROS_INFO("The distance to obstacle 1 : %f",obs_dist[0]);
-    ROS_INFO("The distance to obstacle 2 : %f",obs_dist[1]);
-    ROS_INFO("The distance to obstacle 3 : %f",obs_dist[2]);
-    ROS_INFO("The distance to obstacle 4 : %f",obs_dist[3]);
-    ROS_INFO("The distance to obstacle 5 : %f",obs_dist[4]);
-}*/
+    if(obs_dist.size()!=0)
+    {
+        ROS_INFO("inside callbak");
+        ROS_INFO("The distance to obstacle 1 : %f",obs_dist[0]);
+        ROS_INFO("The distance to obstacle 2 : %f",obs_dist[1]);
+        ROS_INFO("The distance to obstacle 3 : %f",obs_dist[2]);
+        ROS_INFO("The distance to obstacle 4 : %f",obs_dist[3]);
+        ROS_INFO("The distance to obstacle 5 : %f",obs_dist[4]);
+    }
+}
 
 /*void stop_drone(DJIDrone* drone)
 {
@@ -35,22 +38,35 @@ int main(int argc,char** argv)
     ros::Subscriber dist_sub;
     DJIDrone* drone = new DJIDrone(nh);
     drone->request_sdk_permission_control();
+    ROS_INFO("Acquired permission");
     drone->takeoff();
+    ROS_INFO("Taking off");
+    usleep(40000);
     drone->attitude_control(0x40, 0, 2, 0, 0);
+    ROS_INFO("MOving forward");
 
 
 
-  //  dist_sub = nh.subscribe<sensor_msgs::LaserScan>("/guidance/obstacle_distance",10,get_dist);
+  dist_sub = nh.subscribe<sensor_msgs::LaserScan>("/guidance/obstacle_distance",10,get_dist);
     //ros::spinOnce();
    while(1)
     {
         ros::spinOnce();
-        /*if (obs_dist[1] < 0.5)
-        {
+        if (obs_dist.size() != 0)
+            {
+                if (obs_dist[1] < 0.5)
+                    {
 
-             drone->attitude_control(0x40, 0, 0, 0, 0);
-             usleep(20000);
-        }*/
+                         drone->attitude_control(0x40, 0, 0, 0, 0);
+                         ROS_INFO("OBSTACLE DETECTED!!\nstopping drone!!");
+                         usleep(20000);
+                         drone->landing();
+                         usleep(20000);
+                         break;
+                    }
+
+            }
+
     }
     return 0;
 
